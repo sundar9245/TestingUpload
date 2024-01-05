3059c3da00b67398a7f8821645f1b639e689bb59
sundar9245 <37608854+sundar9245@users.noreply.github.com>
2024-01-05 22:33:07 +0530

    Update python_get_db.py

--- python_get_db.py
+++ python_get_db.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 def get_database():
  
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-   CONNECTION_STRING = "mongodb+srv://sundar_963db:YQLkVe2Y3KVyktEm@cluster963.6rmb1.mongodb.net/test"
+   CONNECTION_STRING = ""
  
    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
    client = MongoClient(CONNECTION_STRING)
